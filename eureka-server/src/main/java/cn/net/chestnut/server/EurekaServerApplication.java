c3da003365c00c8dae95b45132a8c4e1141f973e
tarzan <onlycreator@hotmail.com>
2019-06-27 17:13:31 +0800

    add eureka-server

---
+++ eureka-server/src/main/java/cn/net/chestnut/server/EurekaServerApplication.java
@@ -0,0 +1,16 @@
+package cn.net.chestnut.server;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaServerApplication {
+
+    public static void main(String[] args) {
+        new SpringApplicationBuilder(EurekaServerApplication.class).web(true).run(args);
+    }
+
+}
+
---
+++ eureka-server/src/main/java/cn/net/chestnut/server/IndexController.java
@@ -0,0 +1,22 @@
+package cn.net.chestnut.server;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Description
+ * @Author tarzan
+ * @Date 2019/3/22 9:49 AM
+ **/
+@RestController
+public class IndexController {
+
+    @Value("${eureka.instance.hostname}")
+    private String hostName;
+
+    @RequestMapping("/host")
+    public String index(){
+        return "eureka.instance.hostname="+hostName;
+    }
+}
