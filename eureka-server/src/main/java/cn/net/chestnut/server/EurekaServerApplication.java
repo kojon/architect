0c8dc725a1e979bb273da9af6d7c727eff6dae0f
tarzan <onlycreator@hotmail.com>
2019-06-27 17:30:25 +0800

    使用springboot 2.x

--- eureka-server/src/main/java/cn/net/chestnut/server/EurekaServerApplication.java
+++ eureka-server/src/main/java/cn/net/chestnut/server/EurekaServerApplication.java
@@ -1,7 +1,7 @@
 package cn.net.chestnut.server;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @EnableEurekaServer
@@ -9,7 +9,7 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 public class EurekaServerApplication {
 
     public static void main(String[] args) {
-        new SpringApplicationBuilder(EurekaServerApplication.class).web(true).run(args);
+        SpringApplication.run(EurekaServerApplication.class,args);
     }
 
 }
